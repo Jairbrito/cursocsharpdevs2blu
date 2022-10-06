59f651545bde17ec9e563265889e0cdb900772cc
Jairbrito <brdbrnjairdebrito@outlook.com>
2022-10-05 21:01:38 -0300

    Atualizacao Forms

--- BancoDados/Aula4/New folder/Solution1/src/Devs2Blu.ProjetosAula.SistemaCadastro.Forms/Form1.Designer.cs
+++ BancoDados/Aula4/New folder/Solution1/src/Devs2Blu.ProjetosAula.SistemaCadastro.Forms/Form1.Designer.cs
@@ -28,21 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboConvenio = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCGCCPF = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.groupBox1.Controls.Add(this.lblCGCCPF);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cboConvenio);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(386, 258);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cadastro Form";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Convenio";
+            // 
+            // cboConvenio
+            // 
+            this.cboConvenio.FormattingEnabled = true;
+            this.cboConvenio.Location = new System.Drawing.Point(64, 24);
+            this.cboConvenio.Name = "cboConvenio";
+            this.cboConvenio.Size = new System.Drawing.Size(272, 21);
+            this.cboConvenio.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Location = new System.Drawing.Point(9, 54);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(371, 45);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tipo pessoa";
+            this.groupBox2.Enter += new System.EventHandler(this.Form1_Load);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(184, 16);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(61, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Juridica";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(126, 16);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(52, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Fisica";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Nome";
+            // 
+            // lblCGCCPF
+            // 
+            this.lblCGCCPF.AutoSize = true;
+            this.lblCGCCPF.Location = new System.Drawing.Point(9, 126);
+            this.lblCGCCPF.Name = "lblCGCCPF";
+            this.lblCGCCPF.Size = new System.Drawing.Size(49, 13);
+            this.lblCGCCPF.TabIndex = 9;
+            this.lblCGCCPF.Text = "CGCCPF";
+            this.lblCGCCPF.Visible = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.Menu;
+            this.groupBox3.Location = new System.Drawing.Point(450, 13);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(338, 425);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Endereco/Contato";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Sistema de Cadastro Hospitalar";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboConvenio;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCGCCPF;
+        private System.Windows.Forms.GroupBox groupBox3;
     }
 }
 
--- BancoDados/Aula4/New folder/Solution1/src/Devs2Blu.ProjetosAula.SistemaCadastro.Forms/Form1.cs
+++ BancoDados/Aula4/New folder/Solution1/src/Devs2Blu.ProjetosAula.SistemaCadastro.Forms/Form1.cs
@@ -15,6 +15,7 @@ namespace Devs2Blu.ProjetosAula.SistemaCadastro.Forms
     public partial class Form1 : Form
     {
         public MySqlConnection Conn { get; set; }
+        public ConvenioRepository convenioRepository = new ConvenioRepository();
         public Form1()
         {
             InitializeComponent();
@@ -22,12 +23,49 @@ namespace Devs2Blu.ProjetosAula.SistemaCadastro.Forms
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Conn = ConnectionMySQL.GetConnection();
+            #region TesteConexao
+            /*Conn = ConnectionMySQL.GetConnection();
 
             if (Conn.State == ConnectionState.Open)
             {
-                MessageBox.Show("Connection");
+                MessageBox.Show("Conexão efetuada com sucesso!", "Conexão ao MySQL", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 Conn.Close();
+            }*/
+            #endregion
+            PopulaComboConvenio();
+        }
+        #region Methods
+        private void PopulaComboConvenio()
+        {
+            var listConvenios = convenioRepository.FetchAll();
+
+            while (listConvenios.Read())
+            {
+                cboConvenio.Items.Add(listConvenios.GetString("nome"));
+            }
+        }
+        #endregion
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                lblCGCCPF.Text = "CPF";
+                lblCGCCPF.Visible = true;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                lblCGCCPF.Text = "CNPJ";
+                lblCGCCPF.Visible = true;
             }
         }
     }
